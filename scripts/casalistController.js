1681cc456557b443a351eb3010e916846bef2e80
niiccolas <niiccolas@protonmail.com>
2018-07-10 15:21:48 +0200

    Add angular http service to query mock endpoint

--- scripts/casalistController.js
+++ scripts/casalistController.js
@@ -1,6 +1,12 @@
 // Reference the 'casaList' module defined in /app.js
 angular
   .module('casaList')
-  .controller('casalistController', ($scope, casalistFactory) => { // inject the $scope object in controller
-    $scope.casas = casalistFactory.getCasalist();
+  .controller('casalistController', function ($scope, casalistFactory) { // inject the $scope object in controller
+    $scope.casas;
+
+    casalistFactory.getCasalist().success(function (data) {
+      $scope.casas = data;
+    }).error(function (error) {
+      console.log(error);
+    });
   });
